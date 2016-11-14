63b1f08afb7d671034519ee793181aa1dce9e8b3
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-14 16:36:31 +0800

    v1.1.1

--- SimpleDemo/src/main/java/com/itheima/simpledemo/SimpleDemo3.java
+++ SimpleDemo/src/main/java/com/itheima/simpledemo/SimpleDemo3.java
@@ -1,9 +1,7 @@
 package com.itheima.simpledemo;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.itheima.loopviewpager.LoopViewPager;
 
@@ -17,20 +15,6 @@ public class SimpleDemo3 extends AppCompatActivity {
         setContentView(R.layout.activity_simple_demo3);
         loopViewPager = (LoopViewPager) findViewById(R.id.lvp_pager);
         loopViewPager.setImgAndTitleData(DataFactory.imgListString(), DataFactory.titleListString());
-        loopViewPager.setPageTransformer(1500, new ViewPager.PageTransformer() {
-            @Override
-            public void transformPage(View view, float position) {
-                if (position <= 0) {
-                    view.setPivotX(view.getMeasuredWidth());
-                    view.setPivotY(view.getMeasuredHeight() * 0.5f);
-                    view.setRotationY(90f * position);
-                } else if (position <= 1) {
-                    view.setPivotX(0);
-                    view.setPivotY(view.getMeasuredHeight() * 0.5f);
-                    view.setRotationY(90f * position);
-                }
-            }
-        });
     }
 
 }
