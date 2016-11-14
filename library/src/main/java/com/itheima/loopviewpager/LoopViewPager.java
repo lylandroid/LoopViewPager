8cf893b5151ba81f680b9a5d457602cfabd273ee
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-14 12:06:54 +0800

    v1.1.1

--- library/src/main/java/com/itheima/loopviewpager/LoopViewPager.java
+++ library/src/main/java/com/itheima/loopviewpager/LoopViewPager.java
@@ -12,56 +12,40 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.itheima.loopviewpager.transformer.BaseTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoopViewPager<A, B> extends FrameLayout implements View.OnTouchListener {
 
-    private final int Accordion = 1;
-    private final int Background2Foreground = 2;
-    private final int CubeIn = 3;
-    private final int DepthPage = 4;
-    private final int Fade = 5;
-    private final int FlipHorizontal = 6;
-    private final int FlipPage = 7;
-    private final int Foreground2Background = 8;
-    private final int RotateDown = 9;
-    private final int RotateUp = 10;
-    private final int Stack = 11;
-    private final int Tablet = 12;
-    private final int ZoomIn = 13;
-    private final int ZoomOutSlide = 14;
-    private final int ZoomOut = 15;
+    private final int MIN_TIME = 500;//最小轮播间隔时间
+    private final int SCROLL = 1;//轮播消息常量
 
-    private final int MIN_TIME = 500;
-    private int intervalTime;//轮播时间，默认3秒
-    private int animTime;//动画时间
-    private int animStyle;//动画样式
-    private boolean scrollEnable;//是否可以手动滚动
-    private boolean touchEnable;//触摸是否停止
+    private A imgData;//图片数据，数组（int,String），集合（int,String）
+    private B titleData;//标题数据，数组（String），集合（String）
+    private int imgLength;//图片数据长度
+    private int titleLength;//标题数据长度
+
+    private int intervalTime;//轮播时间
+    private boolean scrollEnable;//是否禁用滚动
+    private boolean touchEnable;//是否禁用触摸
     private CustomViewPager viewPager;//轮播页面
+
     private List<LoopDotsView> loopDotsViews = new ArrayList<>();//轮播圆点
     private List<LoopTitleView> loopTitleViews = new ArrayList<>();//轮播文本
 
-    private A imgData;//图片数据
-    private B titleData;//标题数据
-    private int length;//数据长度
-    private int vpIndex;//页面索引
-    private int dotIndex;//圆点索引
-    private final int SCROLL = 1;//轮播消息
+    private int realIndex;//真实的索引
+    private int showIndex;//展示的索引
 
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case SCROLL:
-                    vpIndex++;
-                    viewPager.setCurrentItem(vpIndex);
+                    realIndex++;
+                    viewPager.setCurrentItem(realIndex);
                     handler.sendEmptyMessageDelayed(SCROLL, intervalTime);
                     break;
             }
@@ -70,130 +54,119 @@ public class LoopViewPager<A, B> extends FrameLayout implements View.OnTouchList
 
     public LoopViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoopViewPager);
-        intervalTime = typedArray.getInt(R.styleable.LoopViewPager_intervalTime, 0);
-        animTime = typedArray.getInt(R.styleable.LoopViewPager_animTime, 0);
-        animStyle = typedArray.getInt(R.styleable.LoopViewPager_animStyle, 0);
-
-        scrollEnable = typedArray.getBoolean(R.styleable.LoopViewPager_scrollEnable, true);
-        touchEnable = typedArray.getBoolean(R.styleable.LoopViewPager_touchEnable, true);
-        if (intervalTime < MIN_TIME && intervalTime > 0) {
-            intervalTime = MIN_TIME;
-        }
-        typedArray.recycle();
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.LoopViewPager);
+        intervalTime = a.getInt(R.styleable.LoopViewPager_intervalTime, 0);
+        scrollEnable = a.getBoolean(R.styleable.LoopViewPager_scrollEnable, true);
+        touchEnable = a.getBoolean(R.styleable.LoopViewPager_touchEnable, true);
+        a.recycle();
         View.inflate(getContext(), R.layout.weight_loopviewpager, this);
         viewPager = (CustomViewPager) findViewById(R.id.cvp_pager);
-        if (animStyle > 0) {
-            BaseTransformer t = null;
-            switch (animStyle) {
-//                case Accordion:
-//                    t = new AccordionTransformer();
-//                    break;
-//                case Background2Foreground:
-//                    t = new BackgroundToForegroundTransformer();
-//                    break;
-//                case CubeIn:
-//                    t = new CubeInTransformer();
-//                    break;
-//                case DepthPage:
-//                    t = new DepthPageTransformer();
-//                    break;
-//                case Fade:
-//                    t = new FadeTransformer();
-//                    break;
-//                case FlipHorizontal:
-//                    t = new FlipHorizontalTransformer();
-//                    break;
-//                case FlipPage:
-//                    t = new FlipPageViewTransformer();
-//                    break;
-//                case Foreground2Background:
-//                    t = new ForegroundToBackgroundTransformer();
-//                    break;
-//                case RotateDown:
-//                    t = new RotateDownTransformer();
-//                    break;
-//                case RotateUp:
-//                    t = new RotateUpTransformer();
-//                    break;
-//                case Stack:
-//                    t = new StackTransformer();
-//                    break;
-//                case Tablet:
-//                    t = new TabletTransformer();
-//                    break;
-//                case ZoomIn:
-//                    t = new ZoomInTransformer();
-//                    break;
-//                case ZoomOutSlide:
-//                    t = new ZoomOutSlideTransformer();
-//                    break;
-//                case ZoomOut:
-//                    t = new ZoomOutTransformer();
-//                    break;
-                default:
-                    Toast.makeText(getContext(), "动画效果未实现", Toast.LENGTH_SHORT).show();
-                    break;
-            }
-            if (t != null)
-                viewPager.setPageTransformer(true, t);
+        if (intervalTime > 0 && intervalTime < MIN_TIME) {
+            intervalTime = MIN_TIME;
         }
     }
 
-    public void setPageTransformer(ViewPager.PageTransformer t){
-        viewPager.setPageTransformer(true, t);
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        if (touchEnable && intervalTime >= MIN_TIME) {
+            switch (motionEvent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    handler.removeCallbacksAndMessages(null);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    handler.sendEmptyMessageDelayed(SCROLL, intervalTime);
+                    break;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (scrollEnable) {
+            return super.onInterceptTouchEvent(ev);
+        } else {
+            return true;
+        }
+    }
+
+    public ViewPager getViewPager() {
+        return viewPager;
     }
 
     public void setImgData(A imgData) {
-        this.imgData = imgData;
-        if (imgData instanceof List) {
-            length = ((List) imgData).size();
-        } else {
-            if (imgData instanceof String[]) {
-                length = ((String[]) imgData).length;
+        imgLength = 0;
+        if (imgData != null) {
+            this.imgData = imgData;
+            if (imgData instanceof List) {
+                imgLength = ((List) imgData).size();
             } else {
-                length = ((int[]) imgData).length;
+                if (imgData instanceof String[]) {
+                    imgLength = ((String[]) imgData).length;
+                } else if (imgData instanceof int[]) {
+                    imgLength = ((int[]) imgData).length;
+                }
             }
         }
-        init();
+        if (imgLength > 0) {
+            if (titleLength > 0 && imgLength != titleLength) {
+                throw new IllegalArgumentException("imgData or titleData is illegal parameter");
+            } else {
+                init();
+            }
+        } else {
+            throw new IllegalArgumentException("imgData is illegal parameter");
+        }
     }
 
     public void setImgAndTitleData(A imgData, B titleData) {
-        this.titleData = titleData;
-        setImgData(imgData);
+        titleLength = 0;
+        if (titleData != null) {
+            this.titleData = titleData;
+            if (titleData instanceof List) {
+                titleLength = ((List) titleData).size();
+            } else {
+                if (titleData instanceof String[]) {
+                    titleLength = ((String[]) titleData).length;
+                }
+            }
+        }
+        if (titleLength > 0) {
+            setImgData(imgData);
+        } else {
+            throw new IllegalArgumentException("titleData is illegal parameter");
+        }
     }
 
     /**
      * 初始化数据
      */
     private void init() {
+        handler.removeCallbacksAndMessages(null);
         loopTitleViews.clear();
         loopDotsViews.clear();
         for (int i = 0; i < getChildCount(); i++) {
             View view = getChildAt(i);
             if (view instanceof LoopTitleView) {
-                loopTitleViews.add((LoopTitleView) view);
+                LoopTitleView loopTitleView = (LoopTitleView) view;
+                loopTitleViews.add(loopTitleView);
             } else if (view instanceof LoopDotsView) {
                 LoopDotsView loopDotsView = (LoopDotsView) view;
-                loopDotsView.initDots(length);
+                loopDotsView.initDots(imgLength);
                 loopDotsViews.add(loopDotsView);
             }
         }
-        vpIndex = 1000 * (Integer.MAX_VALUE % length);
-        dotIndex = -1;
+        realIndex = 1000 * (Integer.MAX_VALUE % imgLength);
+        showIndex = 0;
         viewPager.setAdapter(new LoopPagerAdapter());
-        viewPager.addOnPageChangeListener(new LoopPageChangeListener());
+        viewPager.setCurrentItem(realIndex);
         viewPager.setOnTouchListener(this);
-        viewPager.setCurrentItem(vpIndex);
+        viewPager.addOnPageChangeListener(new LoopPageChangeListener());
         if (intervalTime >= MIN_TIME) {
             handler.sendEmptyMessageDelayed(SCROLL, intervalTime);
         }
     }
 
-
-    /**
-     * 创建适配器
-     */
     private class LoopPagerAdapter extends PagerAdapter {
 
         @Override
@@ -208,14 +181,14 @@ public class LoopViewPager<A, B> extends FrameLayout implements View.OnTouchList
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            int index = position % length;
+            int index = position % imgLength;
             ImageView view = new ImageView(getContext());
             if (imgData instanceof List) {
                 Glide.with(getContext()).load(((List) imgData).get(index)).centerCrop().into(view);
             } else {
                 if (imgData instanceof String[]) {
                     Glide.with(getContext()).load(((String[]) imgData)[index]).centerCrop().into(view);
-                } else {
+                } else if (imgData instanceof int[]) {
                     Glide.with(getContext()).load(((int[]) imgData)[index]).centerCrop().into(view);
                 }
             }
@@ -236,12 +209,11 @@ public class LoopViewPager<A, B> extends FrameLayout implements View.OnTouchList
     private class LoopPageChangeListener implements ViewPager.OnPageChangeListener {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
         }
 
         @Override
         public void onPageSelected(int position) {
-            int index = position % length;
+            int index = position % imgLength;
             if (loopTitleViews.size() > 0 && titleData != null) {
                 for (LoopTitleView loopTitleView : loopTitleViews) {
                     if (titleData instanceof List) {
@@ -251,45 +223,17 @@ public class LoopViewPager<A, B> extends FrameLayout implements View.OnTouchList
                     }
                 }
             }
-            if (loopDotsViews.size() > 0) {
+            if (loopDotsViews.size() > 0 && imgData != null) {
                 for (LoopDotsView loopDotsView : loopDotsViews) {
-                    loopDotsView.update(index, dotIndex);
+                    loopDotsView.update(index, showIndex);
                 }
             }
-            vpIndex = position;
-            dotIndex = index;
+            realIndex = position;
+            showIndex = index;
         }
 
         @Override
         public void onPageScrollStateChanged(int state) {
-
-        }
-    }
-
-    /**
-     * 触摸停止播放
-     */
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        if (intervalTime >= MIN_TIME && touchEnable) {
-            switch (motionEvent.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    handler.removeCallbacksAndMessages(null);
-                    break;
-                case MotionEvent.ACTION_UP:
-                    handler.sendEmptyMessageDelayed(SCROLL, intervalTime);
-                    break;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        if (scrollEnable) {
-            return super.onInterceptTouchEvent(ev);
-        } else {
-            return true;
         }
     }
 
