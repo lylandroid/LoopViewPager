297d14ebb80c8ff143bb2f5b7c463b44d720ac33
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-18 11:20:42 +0800

    v1.2.0

---
+++ library/src/main/java/com/itheima/loopviewpager/LoopDefault.java
@@ -0,0 +1,11 @@
+package com.itheima.loopviewpager;
+
+import com.itheima.loopviewpager.dots.DotStyle;
+
+public interface LoopDefault {
+
+    int dotShape = DotStyle.RECTANGLE;
+    int dotColor  = 0xFFFFFFFF;
+    int dotSelectColor  = 0x55000000;
+
+}
