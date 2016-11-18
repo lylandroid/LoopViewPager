faa65404d3fbe97409b7182f5ed51a2640a1ffad
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-18 11:14:34 +0800

    v1.2.0

---
+++ library/src/main/java/com/itheima/loopviewpager/dots/DotRectangleView.java
@@ -0,0 +1,11 @@
+package com.itheima.loopviewpager.dots;
+
+import android.content.Context;
+
+public class DotRectangleView extends DotBaseView {
+
+    public DotRectangleView(Context context) {
+        super(context);
+    }
+
+}
---
+++ library/src/main/java/com/itheima/loopviewpager/dots/DotTriangleView.java
@@ -0,0 +1,11 @@
+package com.itheima.loopviewpager.dots;
+
+import android.content.Context;
+
+public class DotTriangleView extends  DotBaseView {
+
+    public DotTriangleView(Context context) {
+        super(context);
+    }
+
+}
