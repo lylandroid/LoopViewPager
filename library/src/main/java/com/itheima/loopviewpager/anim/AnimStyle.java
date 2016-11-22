87f71cc7faf35b3417563d22202c3d08b33d7b60
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-22 11:09:00 +0800

    v1.2.2

--- library/src/main/java/com/itheima/loopviewpager/anim/AnimStyle.java
+++ library/src/main/java/com/itheima/loopviewpager/anim/AnimStyle.java
@@ -3,6 +3,8 @@ package com.itheima.loopviewpager.anim;
 public interface AnimStyle {
 
     int ACCORDION = 1;
-    int CUBE = 2;
+    int ACCORDION_UP = 2;
+    int CUBE = 3;
+    int CUBE_UP = 4;
 
 }
