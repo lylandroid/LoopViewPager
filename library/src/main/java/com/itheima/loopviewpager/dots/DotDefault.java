9f54299888798b4f2275851600b9e6654af684ef
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-22 10:48:03 +0800

    v1.2.2

--- library/src/main/java/com/itheima/loopviewpager/dots/DotDefault.java
+++ library/src/main/java/com/itheima/loopviewpager/dots/DotDefault.java
@@ -1,11 +1,9 @@
 package com.itheima.loopviewpager.dots;
 
-import com.itheima.loopviewpager.dots.DotStyle;
-
 public interface DotDefault {
 
     int dotShape = DotStyle.RECTANGLE;
-    int dotColor  = 0x55000000;
-    int dotSelectColor  = 0xFFFFFFFF;
+    int dotColor = 0x55000000;
+    int dotSelectColor = 0xFFFFFFFF;
 
 }
