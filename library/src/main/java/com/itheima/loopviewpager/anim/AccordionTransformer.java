0df554f831edde77e537c27596b6487aaa292061
yaoshouyun <yaoshouyun@itcast.cn>
2016-11-22 10:45:56 +0800

    v1.2.2

--- library/src/main/java/com/itheima/loopviewpager/anim/AccordionTransformer.java
+++ library/src/main/java/com/itheima/loopviewpager/anim/AccordionTransformer.java
@@ -1,12 +1,18 @@
 package com.itheima.loopviewpager.anim;
 
-import android.support.v4.view.ViewPager;
 import android.view.View;
 
-public class AccordionTransformer implements ViewPager.PageTransformer {
+/**
+ * 折叠
+ */
+public class AccordionTransformer extends BasePageTransformer {
+
+    public AccordionTransformer(int orientation) {
+        super(orientation);
+    }
 
     @Override
-    public void transformPage(View view, float position) {
+    public void transformViewPage(View view, float position) {
         view.setPivotX(position <= 0 ? view.getMeasuredWidth() : 0);
         view.setScaleX(position <= 0 ? 1f + position : 1f - position);
     }
--- library/src/main/java/com/itheima/loopviewpager/anim/CubeTransformer.java
+++ library/src/main/java/com/itheima/loopviewpager/anim/CubeTransformer.java
@@ -1,12 +1,18 @@
 package com.itheima.loopviewpager.anim;
 
-import android.support.v4.view.ViewPager;
 import android.view.View;
 
-public class CubeTransformer implements ViewPager.PageTransformer {
+/**
+ * 立方体
+ */
+public class CubeTransformer extends BasePageTransformer {
+
+    public CubeTransformer(int orientation) {
+        super(orientation);
+    }
 
     @Override
-    public void transformPage(View view, float position) {
+    public void transformViewPage(View view, float position) {
         view.setPivotX(position <= 0 ? view.getMeasuredWidth() : 0);
         view.setPivotY(view.getMeasuredHeight() * 0.5f);
         view.setRotationY(90f * position);
